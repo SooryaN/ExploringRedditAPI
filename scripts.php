af59fadad0523922164bae4150e92bc34b595068
SooryaN <nayaran.ayroos@gmail.com>
2015-06-23 03:45:34 +0300

    Now able to retrieve all required stats

--- scripts.php
+++ scripts.php
@@ -1,52 +1,41 @@
 <?php
-$months = array('January','February','March','April','May','June','July','August','September','October','November','December'); 
-$w_days = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
-$hour_arr=array('12am','1am','2am','3am','4am','5am','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm');
-$username = $_POST['username'];
-
-if ($username=""){
-	header("Location: index2.html");
-	die();
-}
-else{
-	start($_POST['username']);
-}
-$user = array();
-	$ratios =  array();
-	$time_day_arr =array();
-	$time_day_arr['list'] = array();
-	$subreddits =  array();
-	$subreddits['list']= array();
-	$posts = array();
-	$self_posts = array();
-	$comments = array();
+$hour_arr=['12am','1am','2am','3am','4am','5am','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm'];
+echo "hi";
+$username="1wheel";
+start($username);
 function start($username){
-	
-	//SET URL!!!
-	
+	global $user,$time_day_arr,$subreddits,$posts,$self_posts,$comments;
+	$user = array();
+	$time_day_arr =  array();
+	$GLOBALS['time_day_arr']['list'] = array(); 
+	$GLOBALS['subreddits'] =  array();
+	$GLOBALS['subreddits']['list'] = array();
+	$GLOBALS['posts'] =  array();
+	$GLOBALS['self_posts'] =  array();
+	$GLOBALS['comments'] =  array();
 	getAbout($username);
 	getAllSub($username);
 }
-
 function getAbout($username) {
 	$json="http://www.reddit.com/user/".$username."/about.json";
 	$obj = json_decode(file_get_contents($json),true);
-	if ($obj)
-	{
-	    $user['comment_karma'] = $obj['data']['comment_karma'];
-	    $user['link_karma'] = $obj['data']['link_karma'];
-	    $user['created'] = $obj['data']['created'];
-	    $user['name'] = $obj['data']['name'];
-	}
-	else{print_r(file_get_contents($json));}
+	if($obj){
+	        
+	        $GLOBALS['user']['comment_karma'] = $obj['data']['comment_karma'];
+	        $GLOBALS['user']['link_karma'] = $obj['data']['link_karma'];
+	        $GLOBALS['user']['created'] = $obj['data']['created'];
+	        $GLOBALS['user']['name'] = $obj['data']['name'];
+	       // populate("usr_details");
+	    }
+	else{echo $obj;}
 }
-
 function getAllSub($username,$after=''){
-	$url="http://www.reddit.com/user/".$username."/overview.json?jcount=25&after=".$after;
-	$data=array();
-	$data['after']=$after;
-	$obj = json_decode(file_get_contents($url),true);
-	if($obj) {
+	//$("#posts")']['html("Retrieving more posts'][' Please wait']['");
+	echo ("fn start: ".$after);
+	$json="http://www.reddit.com/user/".$username."/overview.json?count=25&after=".$after;
+	$obj = json_decode(file_get_contents($json),true);
+	if($obj){
+    	echo ("sucess callback started: ".$after);
         for ($i = count($obj['data']['children'])- 1; $i >= 0; $i--) {
         	$thing = array();
 			$thing['id'] = $obj['data']['children'][$i]['data']['id'];
@@ -55,73 +44,88 @@ function getAllSub($username,$after=''){
     		$thing['subreddit'] = $obj['data']['children'][$i]['data']['subreddit'];
     		$thing['created'] = $obj['data']['children'][$i]['data']['created_utc'];
         	if($obj['data']['children'][$i]['kind'] == "t1"){//comment
-        		$comments[]=$thing;
+        		$GLOBALS['comments'][]=$thing;
         	}else if($obj['data']['children'][$i]['kind'] == "t3"){//submission
         		$thing['score'] = $obj['data']['children'][$i]['data']['score'];
         		if($obj['data']['children'][$i]['data']['is_self']){
-	        		$self_posts[]=$thing;
+	        		$GLOBALS['self_posts'][]=$thing;
         		}else{
-	        		$posts[]=$thing;
-				    if($obj['data']['children'][$i]['data']['ups']== 0){
-	    				$ratio = 0;
-	        		}else if ($obj['data']['children'][$i]['data']['downs']==0){
-	        			$ratio = $obj['data']['children'][$i]['data']['ups'];
-	        		}else{
-	        			$ratio = $obj['data']['children'][$i]['data']['ups']/$obj['data']['children'][$i]['data']['downs'];
-	        		}
-		        	$ratios[]=$ratio;
+	        		$GLOBALS['posts'][]=$thing;
+				    
         		}
         	}
         }
-        if($obj['data']['after'] || $obj['data']['before']){
-	        getAllSub($username,$obj['data']['after']);
-        }else if(!$obj['data']['after'] && !$obj['data']['before']){
-        	$self_posts = isset($self_posts) ? $self_posts : '';
-  	        getAllSub($username,$obj['data']['after']);
-        }else if(!$obj['data']['after'] && !$obj['data']['before']){
-          	//$self_posts = isset($self_posts) ? $self_posts : '';
-	        echo("posts: ");var_dump($posts);echo("self: ");var_dump($self_posts);echo("comments: ");var_dump($comments);
-        	/*processSubs();
-			processSubreddit();
-    		processTime_Day();*/
-        }
-    }
-    else{
-	    	echo("error");
+	        if($obj['data']['after'] || $obj['data']['before']){
+	        	echo ("More Posts");
+	        	processSubs();
+				processSubreddit();
+        		processTime_Day();
+		        getAllSub($username,$obj['data']['after']);
+	        }else if(!$obj['data']['after']){
+	        	echo ("No More Entries");
+	        	
+	        	
+	        }
 	    }
-	}
+	else{echo $obj;}
+	    	
+	    }
 function processSubs(){
-	if ( (count($posts)==0) || (count($self_posts==0)) || count($comments==0) ){
-		echo("no_listing");
+	if ( ((count($GLOBALS['posts'])==0) && (count($GLOBALS['self_posts'])==0)) || (count($GLOBALS['comments'])==0) ){
+		populate("no_listing");
 	}
 	else{
-		$user['num_posts'] = count($posts);
-		$user['num_self_posts'] = count($self_posts);
-		$user['num_comments'] = count($comments);
-		$user['post_up_total']=0;
-		$user['post_down_total']=0;
-		foreach($posts as $key => $value){
-			$user['post_up_total']+= $posts[$key]['ups'];
-			$user['post_down_total']+= $posts[$key]['downs'];
-		}
+		$user['num_posts'] = count($GLOBALS['posts']);
+		$user['num_self_posts'] = count($GLOBALS['self_posts']);
+		$user['num_comments'] = count($GLOBALS['comments']);
+		populate("stats");
 	}
 }
-
-
-?>
-
-<html> 
-    <head> 
-        <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
-        <title>reddit User Stats</title>
-        <link rel="stylesheet" href="main.css">
-        <!--[if IE]><script type="text/javascript" src="js/vendor/excanvas.min.js"></script><![endif]-->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    </head>
-    <body>
-<form id="form" method="post" action="scripts.php" accept-charset='UTF-8'>
-                <input type="text" style="width:200px;" name="username" placeholder="reddit Username" value="" required autofocus/>
-                <input type="submit" value="Go" class="button" name="submit">
-            </form>
-    </body>
-</html>
+function processTime_Day(){
+	for ($i =count($GLOBALS['posts']) - 1; $i >= 0; $i--) {$GLOBALS['time_day_arr']['list'][]=$GLOBALS['posts'][$i]['created'];}
+	for ($i = count($GLOBALS['self_posts']) - 1; $i >= 0; $i--) {$GLOBALS['time_day_arr']['list'][]=$GLOBALS['self_posts'][$i]['created'];}
+	for ($i = count($GLOBALS['comments']) - 1; $i >= 0; $i--) {$GLOBALS['time_day_arr']['list'][]=$GLOBALS['comments'][$i]['created'];}
+	$GLOBALS['time_day_arr']['repeats_hour'] = array();
+	$GLOBALS['time_day_arr']['repeats_week_day'] = array();
+	for ($i = count($GLOBALS['time_day_arr']['list']) - 1; $i >= 0; $i--) {
+		$date=$GLOBALS['time_day_arr']['list'][$i]*1000;
+		$hour = strval(date( "G", $date));
+		$day = date( "j", $date);
+		$week_day = date( "l", $date);
+		$month = date( "F", $date);
+		$year = date( "Y", $date);
+		if(isset($GLOBALS['time_day_arr']['repeats_hour'][$hour])){ //already in 
+			$GLOBALS['time_day_arr']['repeats_hour'][$hour]++;
+			//echo ($GLOBALS['time_day_arr']['list[i])
+		}else{
+			$GLOBALS['time_day_arr']['repeats_hour'][$hour]=1;
+		}
+		if(isset($GLOBALS['time_day_arr']['repeats_week_day'][$week_day])) { //already in 
+			$GLOBALS['time_day_arr']['repeats_week_day'][$week_day]++;
+		}else{
+			$GLOBALS['time_day_arr']['repeats_week_day'][$week_day]=1;
+		}
+	};
+	
+	populate("time_day");
+}
+function processSubreddit(){
+	for ($i =count($GLOBALS['posts']) - 1; $i >= 0; $i--) {$GLOBALS['subreddits']['list'][]=$GLOBALS['posts'][$i]['subreddit'];}
+	for ($i = count($GLOBALS['self_posts']) - 1; $i >= 0; $i--) {$GLOBALS['subreddits']['list'][]=$GLOBALS['self_posts'][$i]['subreddit'];}
+	for ($i = count($GLOBALS['comments']) - 1; $i >= 0; $i--) {$GLOBALS['subreddits']['list'][]=$GLOBALS['comments'][$i]['subreddit'];}
+	
+	$GLOBALS['subreddits']['repeats'] = array();
+	for ($i = count($GLOBALS['subreddits']['list']) - 1; $i >= 0; $i--) {
+		if(isset($GLOBALS['subreddits']['repeats'][$GLOBALS['subreddits']['list'][$i]])){ //already in 
+			$GLOBALS['subreddits']['repeats'][$GLOBALS['subreddits']['list'][$i]]++;
+		}else{
+			$GLOBALS['subreddits']['repeats'][$GLOBALS['subreddits']['list'][$i]]=1;
+		}
+	};
+	
+	populate("subreddit");
+}
+function populate($type)
+{
+	echo "populated"."<br>";
+}
