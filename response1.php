0a51270fa3ca2febf7c01cfc3f7f0e6a90d2cb60
SooryaN <nayaran.ayroos@gmail.com>
2015-06-24 19:12:20 +0530

    updated POST parameters

--- response1.php
+++ response1.php
@@ -3,7 +3,7 @@ include("config.php");
  var_dump($_POST['username']);
  $sql = "INSERT INTO Hope (json,Username) VALUES(?,?)";
 	if ($stmt = $mysqli->prepare($sql)) {
-	$stmt->bind_param("ss", $_POST['details'],$_POST['username']);
+	$stmt->bind_param("s", $_POST['details'],$_POST['username']);
 	$stmt->execute();
 	}
 /*$date = date('Y-m-d H:i:s');
